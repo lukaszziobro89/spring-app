ff5ca437c6b0aba2353bf1fe445041c3982299ad
Lukasz Ziobro <lukasz.ziobro.89@gmail.com>
2018-08-13 14:43:43 +0200

    save update

--- src/main/java/pl/student/StudentDAOImpl.java
+++ src/main/java/pl/student/StudentDAOImpl.java
@@ -162,7 +162,7 @@ public class StudentDAOImpl implements StudentDAO{
     @Transactional
     public void truncateTable() {
         Session currentSession = sessionFactory.getCurrentSession();
-        Query theQuery = currentSession.createNativeQuery("truncate table Student");
+        Query theQuery = currentSession.createNativeQuery("truncate table student");
         theQuery.executeUpdate();
     }
 }
